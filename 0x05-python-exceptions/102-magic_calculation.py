7ad5d74ca40888ae3ff164723a16875dce9f82ea
b-lemy <lemabrian1234@gmail.com>
2022-12-20 01:47:42 -0800

    pycodestyle

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -10,7 +10,7 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
             else:
                 result += a ** b / i
-        except (IndexError, ValueError, TypeError):
+        except (TypeError, NameError, ValueError, IndexError):
             result = b + a
             break
     return (result)
