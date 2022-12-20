cf8cfdb7955c0f853d4dd4406b454a5304264746
b-lemy <lemabrian1234@gmail.com>
2022-12-20 01:21:31 -0800

    pycodestyle

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -16,6 +16,6 @@ def safe_function(fct, *args):
     try:
         result = fct(*args)
         return (result)
-    except NameError:
+    except (NameError, ZeroDivisionError):
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
