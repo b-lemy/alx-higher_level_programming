729229c28138405e3d05ac1786c5eed414665253
b-lemy <lemabrian1234@gmail.com>
2023-03-20 01:53:42 -0700

    correction

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -10,6 +10,9 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == '__main__':
+    """
+         the session should join two tables  
+    """
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
