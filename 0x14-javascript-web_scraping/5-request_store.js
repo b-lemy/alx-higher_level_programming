5fbf5987fa33aca3080cc22f3be6daf923fd3edc
b-lemy <lemabrian1234@gmail.com>
2023-04-26 08:51:27 -0700

    web scrapping commits

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 
-const request = require('request');
 const fs = require('fs');
+const request = require('request');
 const url = process.argv[2];
-const filePath = process.argv[3];
 
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    fs.writeFile(filePath, body, 'utf-8');
-  }
+request(url, (error, response, body) => {
+  if (error) throw new Error(error);
+  fs.writeFile(process.argv[3], body, 'utf8', (err) => {
+    if (err) throw new Error(err);
+  });
 });
